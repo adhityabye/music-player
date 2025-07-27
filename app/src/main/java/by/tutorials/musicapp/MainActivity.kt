127ff5a88d689eca378e8be765ee5f3e14b47a56
adhityabye <mr.adhityabayu@gmail.com>
2025-07-27 13:24:40 +0700

    fix(ui): add handler for search bar

--- app/src/main/java/by/tutorials/musicapp/MainActivity.kt
+++ app/src/main/java/by/tutorials/musicapp/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -24,6 +26,7 @@ import by.tutorials.musicapp.ui.components.MusicList
 import by.tutorials.musicapp.viewmodel.SongViewModel
 import by.tutorials.musicapp.viewmodel.SongViewModelFactory
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -39,13 +42,10 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             MusicAppTheme {
-                Scaffold { inner ->
-                    MainScreen(
-                        modifier = Modifier.padding(inner),
-                        viewModel = viewModel,
-                        playbackManager = playbackManager
-                    )
-                }
+                MainScreen(
+                    viewModel = viewModel,
+                    playbackManager = playbackManager
+                )
             }
         }
     }
@@ -59,6 +59,9 @@ fun MainScreen(
 ) {
     val context = LocalContext.current
 
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
     var query by remember { mutableStateOf("") }
     var playingId by remember { mutableStateOf<Long?>(null) }
     var isPlaying by remember { mutableStateOf(false) }
@@ -69,114 +72,137 @@ fun MainScreen(
     val loading by viewModel.loading.collectAsState()
     val error by viewModel.error.collectAsState()
 
-
-
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-
-        SearchBar(
-            text = query,
-            onTextChange = { query = it },
-            onSearch = { viewModel.searchSongs(query) }
-        )
-
-        Box(
-            Modifier
-                .weight(1f)
-                .fillMaxWidth()) {
-            when {
-                loading -> CircularProgressIndicator(Modifier.align(Alignment.Center))
-                error != null -> Text(
-                    text = "error: $error",
-                    color = MaterialTheme.colorScheme.error,
-                    modifier = Modifier.align(Alignment.Center)
-                )
-
-                else -> MusicList(
-                    songs = songs,
-                    playingId = playingId,
-                    onSongClick = { song ->
-                        song.previewUrl?.let { url ->
-                            playbackManager.play(url)
-                        }
-                        playingId = song.id
-                        isPlaying = true
-                        position = 0f
-                        duration = playbackManager.duration().toFloat()
-                    }
-                )
-            }
-        }
-        LaunchedEffect(key1 = isPlaying, key2 = playingId) {
-            if (isPlaying && playingId != null) {
-
-                while (duration == 0f) {
-                    duration = playbackManager.duration().toFloat()
-                    delay(100)
-                }
-
-                while (isPlaying) {
-                    position = playbackManager.currentPosition().toFloat()
-                    delay(300)
-                }
-            }
-        }
-
-        playingId?.let { currentId ->
-            val currentIndex = songs.indexOfFirst { it.id == currentId }
-            val currentSong = songs.getOrNull(currentIndex) ?: return
-
-            PlaybackControls(
-                title = currentSong.title,
-                artist = currentSong.artist,
-                isPlaying = isPlaying,
-                position = position,
-                duration = duration,
-
-                onPrevious = {
-                    if (currentIndex > 0) {
-                        songs[currentIndex - 1].let { prev ->
-                            prev.previewUrl?.let { playbackManager.play(it) }
-                            playingId = prev.id
-                            isPlaying = true
-                        }
-                    } else {
-                        Toast
-                            .makeText(context, "At first list", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-                },
-
-                onPlayPause = {
-                    if (isPlaying) playbackManager.pause()
-                    else playbackManager.resume()
-                    isPlaying = !isPlaying
-                },
-
-                onNext = {
-                    if (currentIndex < songs.lastIndex) {
-                        songs[currentIndex + 1].let { next ->
-                            next.previewUrl?.let { playbackManager.play(it) }
-                            playingId = next.id
-                            isPlaying = true
-                        }
-                    } else {
-                        Toast
-                            .makeText(context, "At end list", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-                },
-
-                onSeek = { newPos ->
-                    playbackManager.seekTo(newPos.toInt())
-                    position = newPos
-                }
-            )
+    fun showMessage(msg: String) {
+        scope.launch {
+            snackbarHostState.showSnackbar(msg)
         }
     }
+
+    Scaffold(
+        modifier     = modifier.fillMaxSize(),
+        snackbarHost = { SnackbarHost(snackbarHostState) }
+    ) { innerPadding ->
+        Column(
+            modifier = modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+
+            SearchBar(
+                text = query,
+                onTextChange = { query = it },
+                onSearch = {
+                    if (query.isBlank()) {
+                        showMessage("Janganlupa masukin nama musik atau penyanyinya yh!")
+                    } else {
+                        viewModel.searchSongs(query)
+                    }
+                }
+            )
+
+            Box(
+                Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+            ) {
+                when {
+                    loading -> CircularProgressIndicator(Modifier.align(Alignment.Center))
+                    error != null -> Text(
+                        text = "error: $error",
+                        color = MaterialTheme.colorScheme.error,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+
+                    songs.isEmpty() && query.isNotBlank() -> Text(
+                        text = "Yahh..sayang lagu yang kamu cari tidak ditemukan",
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+
+                    else -> MusicList(
+                        songs = songs,
+                        playingId = playingId,
+                        onSongClick = { song ->
+                            song.previewUrl?.let { url ->
+                                playbackManager.play(url)
+                            }
+                            playingId = song.id
+                            isPlaying = true
+                            position = 0f
+                            duration = playbackManager.duration().toFloat()
+                        }
+                    )
+                }
+            }
+            LaunchedEffect(key1 = isPlaying, key2 = playingId) {
+                if (isPlaying && playingId != null) {
+
+                    while (duration == 0f) {
+                        duration = playbackManager.duration().toFloat()
+                        delay(100)
+                    }
+
+                    while (isPlaying) {
+                        position = playbackManager.currentPosition().toFloat()
+                        delay(300)
+                    }
+                }
+            }
+
+            playingId?.let { currentId ->
+                val currentIndex = songs.indexOfFirst { it.id == currentId }
+                val currentSong = songs.getOrNull(currentIndex) ?: return@let
+
+                PlaybackControls(
+                    title = currentSong.title,
+                    artist = currentSong.artist,
+                    isPlaying = isPlaying,
+                    position = position,
+                    duration = duration,
+
+                    onPrevious = {
+                        if (currentIndex > 0) {
+                            songs[currentIndex - 1].let { prev ->
+                                prev.previewUrl?.let { playbackManager.play(it) }
+                                playingId = prev.id
+                                isPlaying = true
+                            }
+                        } else {
+                            Toast
+                                .makeText(context, "At first list", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    },
+
+                    onPlayPause = {
+                        if (isPlaying) playbackManager.pause()
+                        else playbackManager.resume()
+                        isPlaying = !isPlaying
+                    },
+
+                    onNext = {
+                        if (currentIndex < songs.lastIndex) {
+                            songs[currentIndex + 1].let { next ->
+                                next.previewUrl?.let { playbackManager.play(it) }
+                                playingId = next.id
+                                isPlaying = true
+                            }
+                        } else {
+                            Toast
+                                .makeText(context, "At end list", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    },
+
+                    onSeek = { newPos ->
+                        playbackManager.seekTo(newPos.toInt())
+                        position = newPos
+                    }
+                )
+            }
+        }
+    }
+
 }
 
